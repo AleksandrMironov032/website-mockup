a9e7c5a056c4a6372956aeeb730b1302288a279e
Moroz <aleksandrmironov032@gmail.com>
2026-02-27 01:44:33 +0600

    Модель слайдера

--- slider/models.py
+++ slider/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from filer.fields.image import FilerImageField
 
-# Create your models here.
+
+class Slide(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Название'
+    )
+    image = FilerImageField(
+        on_delete=models.CASCADE,
+        verbose_name='Фотография',
+        related_name='slides'
+    )
+    order = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Порядок',
+        db_index=True
+    )
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = 'Слайд'
+        verbose_name_plural = 'Слайды'
+
+    def __str__(self):
+        return self.title
